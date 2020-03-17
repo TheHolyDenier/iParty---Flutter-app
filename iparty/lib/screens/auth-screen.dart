6bd55dcd19bbb8b2e7c90fb1e58b44863511ff98
Hele <bloodthirsty.snow.white@gmail.com>
2020-03-17 18:15:34 +0100

    03/17

--- iparty/lib/screens/auth-screen.dart
+++ iparty/lib/screens/auth-screen.dart
@@ -79,7 +79,10 @@ class _AuthScreenState extends State<AuthScreen> {
                         color: _themeOf.accentColor,
                       ),
                     ),
-                    onSaved: (value) => _displayName = value,
+                    onSaved: (value) => _displayName = value.trim(),
+                    validator: (value) => value.isEmpty
+                        ? 'Es necesario escribir un nombre identificativo'
+                        : null,
                   ),
                   emailWidget(),
                   passwordWidget(setModalState),
@@ -109,11 +112,6 @@ class _AuthScreenState extends State<AuthScreen> {
                         // Si todo es correcto
                         form.save();
                         try {
-                          // Inicio de sesión
-                          // FirebaseUser result = await Provider.of<AuthService>(
-                          //         context,
-                          //         listen: false)
-                          //     .loginUser(email: _email, password: _password);
                           FirebaseUser user = (await FirebaseAuth.instance
                                   .createUserWithEmailAndPassword(
                                       email: _email, password: _password))
@@ -127,7 +125,12 @@ class _AuthScreenState extends State<AuthScreen> {
                                 .setData({
                               'email': _email,
                               'displayName': _displayName
-                            }).then((_) => Navigator.of(context).pop());
+                            }).then((_) async {
+                              await Provider.of<
+                                      AuthService>(context, listen: false)
+                                  .loginUser(email: _email, password: _password);
+                                  Navigator.of(context).pop(); 
+                            });
                           });
                         } on AuthException catch (error) {
                           //Manejo de errores
@@ -137,15 +140,14 @@ class _AuthScreenState extends State<AuthScreen> {
                               errorMessage =
                                   'Compruebe que está conectado a Internet y vuelva a intentarlo.';
                             else if (error.code == 'ERROR_EMAIL_ALREADY_IN_USE')
-                             errorMessage =
-                                  'Ese e-mail ya está en uso.';
+                              errorMessage = 'Ese e-mail ya está en uso.';
                           });
                           print('${error.code}: ${error.message}');
                         } on Exception catch (error) {
                           setModalState(() {
                             failedRegister = true;
                             errorMessage =
-                                'Ha habido un error. Vuelva a intentarlo';
+                                'Ha habido un error. Compruebe que tenga acceso a Internet y vuelva a intentarlo';
                           });
                           print(error.toString());
                         }
@@ -167,7 +169,7 @@ class _AuthScreenState extends State<AuthScreen> {
 
 // Genera el botón de inicio de sesión
   Widget _buildLoginButton() {
-    var failedLogging = false;
+    var failedLogging = false, ressetPassword = false, connectionError = false;
     var errorMessage = '';
     return RaisedButton(
       elevation: 0.0,
@@ -190,9 +192,9 @@ class _AuthScreenState extends State<AuthScreen> {
             child: Column(children: <Widget>[
               emailWidget(),
               passwordWidget(setModalState),
-              if (failedLogging)
+              if (failedLogging) // Muestra mensaje de error de internet
                 Padding(
-                  padding: EdgeInsets.only(top: 10.0),
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
@@ -209,6 +211,24 @@ class _AuthScreenState extends State<AuthScreen> {
                     ],
                   ),
                 ),
+              if (failedLogging && !connectionError)
+                ressetPassword //Muestra botón/mensaje de confirmación de recuperación de contraseña
+                    ? Text('E-mail enviado. Revise su correo electrónico.')
+                    : FlatButton(
+                        child: Text(
+                          'Recuperar contraseña',
+                          style: TextStyle(color: _themeOf.errorColor),
+                        ),
+                        onPressed: () => FirebaseAuth.instance
+                            .sendPasswordResetEmail(email: _email)
+                            .then(
+                              (_) => setModalState(
+                                () {
+                                  ressetPassword = true;
+                                },
+                              ),
+                            ),
+                      ),
               RaisedButton(
                 onPressed: () async {
                   final form = _formKey.currentState;
@@ -223,11 +243,18 @@ class _AuthScreenState extends State<AuthScreen> {
                     } on AuthException catch (error) {
                       //Manejo de errores
                       setModalState(() {
-                        failedLogging = true;
-                        errorMessage = error.code ==
-                                'ERROR_NETWORK_REQUEST_FAILED'
-                            ? 'Compruebe que está conectado a Internet y vuelva a intentarlo.'
-                            : 'La contraseña y el e-mail no coinciden';
+                        ressetPassword = false;
+                        if (error.code == 'ERROR_NETWORK_REQUEST_FAILED') {
+                          errorMessage =
+                              'Compruebe que está conectado a Internet y vuelva a intentarlo.';
+                          connectionError = true;
+                          failedLogging = true;
+                        } else {
+                          errorMessage =
+                              'La contraseña y el e-mail no coinciden';
+                          failedLogging = true;
+                          connectionError = false;
+                        }
                       });
                       print('${error.code}: ${error.message}');
                     } on Exception catch (error) {
@@ -298,7 +325,7 @@ class _AuthScreenState extends State<AuthScreen> {
           validator: (value) {
             var error;
             if (value.isEmpty)
-              error = 'Este campo no puede estar vacío';
+              error = 'Es necesario escribir una contraseña';
             else if (value.trim().length < 6)
               error = 'Contraseña demasiado corta';
             return error;
