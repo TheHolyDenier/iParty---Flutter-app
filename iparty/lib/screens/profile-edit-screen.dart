300f191b8b8aeb38296cce598d51789bac28ba7d
Hele <bloodthirsty.snow.white@gmail.com>
2020-04-09 11:01:11 +0200

    half-working-camera-image

--- iparty/lib/screens/profile-edit-screen.dart
+++ iparty/lib/screens/profile-edit-screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:iparty/providers/users.dart';
 import 'package:iparty/widgets/avatar-circles.dart';
 import 'package:iparty/widgets/drawer.dart';
@@ -12,10 +15,13 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  var _imageUrl = '';
+
   @override
   Widget build(BuildContext context) {
     var activeUser =
         Provider.of<UsersProvider>(context, listen: false).activeUser;
+    print(activeUser);
     return Scaffold(
       appBar: AppBar(title: Text('Tu perfil')),
       drawer: MyDrawer(),
@@ -29,7 +35,9 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 Container(
                   height: 200,
                   width: 200,
-                  child: MyTextAvatarCircle('H'),
+                  child: _imageUrl == ''
+                      ? MyTextAvatarCircle('H')
+                      : MyImageAvatarCircle(_imageUrl),
                   //  activeUser.imageUrl.isEmpty
                   //     ? MyTextAvatarCircle(activeUser.displayName[0])
                   //     : MyImageAvatarCircle(activeUser.imageUrl),
@@ -38,8 +46,9 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   bottom: 10,
                   right: 10,
                   child: FloatingActionButton(
-                      onPressed: () {},
-                      child: Icon(Icons.photo_camera, color: Colors.white)),
+                    onPressed: () => _choseImage(context),
+                    child: Icon(Icons.photo_camera, color: Colors.white),
+                  ),
                 ),
               ],
             ),
@@ -67,7 +76,8 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                             decoration: InputDecoration(
                                 icon: Icon(Icons.map), hintText: 'Dirección')),
                         Positioned(
-                          child: Icon(Icons.location_searching, color: Theme.of(context).accentColor),
+                          child: Icon(Icons.location_searching,
+                              color: Theme.of(context).accentColor),
                           right: 0,
                           top: 0,
                           bottom: 0,
@@ -103,5 +113,93 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         )),
       ),
     );
+  }
+
+  Future<String> _choseImage(BuildContext context) async {
+    String imageUrl = '';
+    File image;
+    return showDialog<String>(
+      context: context,
+      barrierDismissible: false,
+      // dialog is dismissible with a tap on the barrier
+      builder: (BuildContext context) {
+        return StatefulBuilder(builder: (context, setDialogState) {
+          return AlertDialog(
+            title: Text('Imagen de perfil'),
+            content: new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(
+                  child: image == null
+                      ? Container(
+                          child: Icon(Icons.photo),
+                          color: Theme.of(context).accentColor,
+                        )
+                      : FittedBox(
+                          child: Image.file(image),
+                          fit: BoxFit.cover,
+                        ),
+                  width: 200,
+                  height: 200,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    _imageSelectButton(Icons.photo, 'Desde galería', () {}),
+                    _imageSelectButton(
+                        Icons.photo_camera,
+                        'Desde la cámara',
+                        () => setDialogState(() async {
+                              image = await _openCamera();
+                              print('tata' + image.toString());
+                            })),
+                  ],
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop(imageUrl);
+                },
+              ),
+            ],
+          );
+        });
+      },
+    );
+  }
+
+  Widget _imageSelectButton(IconData icon, String text, Function function) {
+    return SizedBox(
+      width: 200,
+      child: RaisedButton(
+        onPressed: function,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 5.0),
+              child: Icon(icon),
+            ),
+            Text(text),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //connect camera
+  Future<File> _openCamera() async {
+    print('Picker is Called');
+    File img = await ImagePicker.pickImage(source: ImageSource.camera);
+//    if (img != null) {
+//      setState(() {
+//        image = img;
+//      });
+//    }
+    return img;
   }
 }
