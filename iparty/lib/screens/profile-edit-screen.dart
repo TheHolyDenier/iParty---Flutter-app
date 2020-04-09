972c56e62278f20b86abbfae849ae5d0b2b3612f
Hele <bloodthirsty.snow.white@gmail.com>
2020-04-09 13:02:00 +0200

    Update profile-edit-screen.dart

--- iparty/lib/screens/profile-edit-screen.dart
+++ iparty/lib/screens/profile-edit-screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:iparty/models/user.dart';
 
 import 'package:provider/provider.dart';
 
@@ -17,14 +19,25 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  User _user;
   var _imageUrl = '';
   var _tempUrl = '';
+  final _controllerBio = TextEditingController();
+  final _controllerGeo = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    _controllerBio.dispose();
+    _controllerGeo.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var activeUser =
-        Provider.of<UsersProvider>(context, listen: false).activeUser;
-    print(activeUser); // Instance of user
+    if (_user == null) {
+      _searchActiveUser();
+    }
     return Scaffold(
       appBar: AppBar(title: Text('Tu perfil')),
       drawer: MyDrawer(),
@@ -42,11 +55,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                         ? MyTextAvatarCircle('H')
                         : MyImageAvatarCircle(
                             _tempUrl == '' ? _imageUrl : _tempUrl,
-                            _tempUrl == '')
-                    //  activeUser.imageUrl.isEmpty
-                    //     ? MyTextAvatarCircle(activeUser.displayName[0])
-                    //     : MyImageAvatarCircle(activeUser.imageUrl),
-                    ),
+                            _tempUrl == '')),
                 Positioned(
                   bottom: 10,
                   right: 10,
@@ -67,6 +76,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 child: Column(
                   children: <Widget>[
                     TextFormField(
+                      controller: _controllerBio,
                       decoration: InputDecoration(
                         icon: Icon(Icons.edit),
                         hintText: 'Bio',
@@ -78,6 +88,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                     Stack(
                       children: <Widget>[
                         TextFormField(
+                            controller: _controllerGeo,
                             decoration: InputDecoration(
                                 icon: Icon(Icons.map), hintText: 'Dirección')),
                         Positioned(
@@ -109,7 +120,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                         ),
                       ),
                     ),
-                    RaisedButton(onPressed: () {}, child: Text('guardar'))
+                    RaisedButton(onPressed: _saveData, child: Text('guardar'))
                   ],
                 ),
               ),
@@ -120,12 +131,40 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     );
   }
 
+  _saveData() async {
+//    try {
+//      Firestore.instance.collection('users').document(_user.uid).setData(
+//          {'email': _email, 'displayName': _displayName}).then((_) async {
+//        await Provider.of<AuthService>(context, listen: false)
+//            .loginUser(email: _email, password: _password);
+//        Navigator.of(context).pop();
+//      });
+//    } on AuthException catch (error) {
+//      //Manejo de errores
+//      setModalState(() {
+//        failedRegister = true;
+//        if (error.code == 'ERROR_NETWORK_REQUEST_FAILED')
+//          errorMessage =
+//              'Compruebe que está conectado a Internet y vuelva a intentarlo.';
+//        else if (error.code == 'ERROR_EMAIL_ALREADY_IN_USE')
+//          errorMessage = 'Ese e-mail ya está en uso.';
+//      });
+//      print('${error.code}: ${error.message}');
+//    } on Exception catch (error) {
+//      setModalState(() {
+//        failedRegister = true;
+//        errorMessage =
+//            'Ha habido un error. Compruebe que tenga acceso a Internet y vuelva a intentarlo';
+//      });
+//      print(error.toString());
+//    }
+  }
+
   Future<String> _choseImage(BuildContext context) async {
     String imageUrl = '';
     final result = await showDialog<String>(
       context: context,
       barrierDismissible: true,
-      // dialog is dismissible with a tap on the barrier
       builder: (BuildContext context) {
         return ProfilePicDialog();
       },
@@ -135,5 +174,15 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       _tempUrl = result.toString();
     });
     return imageUrl;
+  }
+
+  void _searchActiveUser() {
+    var provider = Provider.of<UsersProvider>(context, listen: false);
+    if (provider != null) {
+      _user = provider.activeUser;
+    }
+    if (_user.imageUrl != null) {
+      _imageUrl = _user.imageUrl;
+    }
   }
 }
