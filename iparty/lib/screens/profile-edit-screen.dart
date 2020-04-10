1aba67b72cbf79f0d5e002827508e8085a2a7d23
Hele <bloodthirsty.snow.white@gmail.com>
2020-04-10 18:01:13 +0200

    safe??

--- iparty/lib/screens/profile-edit-screen.dart
+++ iparty/lib/screens/profile-edit-screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import '../models/user.dart';
 
@@ -19,18 +20,27 @@ class EditProfileScreen extends StatefulWidget {
   _EditProfileScreenState createState() => _EditProfileScreenState();
 }
 
+enum ErrorsKm { ok, notNumber, outBounds }
+
 class _EditProfileScreenState extends State<EditProfileScreen> {
   User _user;
   var _imageUrl = '';
   var _tempUrl = '';
   final _controllerBio = TextEditingController();
   final _controllerGeo = TextEditingController();
+  final _kmController = TextEditingController();
+  LatLng _latLng;
+  bool _rpg = true;
+  bool _tableGames = true;
+  bool _safeSpace = true;
+  ErrorsKm _errorsKm = ErrorsKm.ok;
 
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
     _controllerBio.dispose();
     _controllerGeo.dispose();
+    _kmController.dispose();
     super.dispose();
   }
 
@@ -81,14 +91,16 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                       decoration: InputDecoration(
                         icon: Icon(Icons.edit),
                         hintText: 'Bio',
+//                        suffix: Text('${_controllerBio.text.length ?? 0}/250'),
                       ),
-                      minLines: 1,
-                      maxLines: 5,
+                      maxLines: 1,
+                      textInputAction: TextInputAction.done,
                       maxLength: 250,
                     ),
                     Stack(
                       children: <Widget>[
                         TextFormField(
+                            enabled: false,
                             controller: _controllerGeo,
                             decoration: InputDecoration(
                                 icon: Icon(Icons.map), hintText: 'Dirección')),
@@ -111,20 +123,79 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                       child: Card(
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Filtros:',
-                                textAlign: TextAlign.left,
-                              ),
-                              Switch.adaptive(value: true, onChanged: null)
-                            ],
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Filtros:',
+                                  textAlign: TextAlign.left,
+                                ),
+                                Center(
+                                  child: Wrap(
+                                    crossAxisAlignment:
+                                        WrapCrossAlignment.center,
+                                    alignment: WrapAlignment.center,
+                                    spacing: 20.0,
+                                    children: <Widget>[
+                                      ChoiceChip(
+                                        label: Text('Rol'),
+                                        selected: _rpg,
+                                        onSelected: (_) {
+                                          setState(() {
+                                            _rpg = !_rpg;
+                                          });
+                                        },
+                                      ),
+                                      ChoiceChip(
+                                        label: Text('Mesa'),
+                                        selected: _tableGames,
+                                        onSelected: (_) {
+                                          setState(() {
+                                            _tableGames = !_tableGames;
+                                          });
+                                        },
+                                      ),
+                                      ChoiceChip(
+                                        label: Text('Espacio seguro'),
+                                        selected: _safeSpace,
+                                        onSelected: (_) {
+                                          setState(() {
+                                            _safeSpace = !_safeSpace;
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  width: double.infinity,
+                                  child: TextField(
+                                    controller: _kmController,
+                                    onChanged: (_) {
+                                      _checkErroKm();
+                                    },
+                                    decoration: new InputDecoration(
+                                        suffix: Text('km'),
+                                        errorText: _errorsKm == ErrorsKm.ok
+                                            ? null
+                                            : _errorsKm == ErrorsKm.notNumber
+                                                ? 'Introduzca un número válido'
+                                                : 'La distancia mínima es 1km'),
+                                    keyboardType:
+                                        TextInputType.numberWithOptions(),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    RaisedButton(onPressed: _saveData, child: Text('guardar'))
+                    RaisedButton(
+                        onPressed: _errorsKm == ErrorsKm.ok ? _saveData : null,
+                        child: Text('guardar'))
                   ],
                 ),
               ),
@@ -145,10 +216,16 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     ref.putFile(File(_tempUrl));
     final url = await ref.getDownloadURL() as String;
     try {
-      Firestore.instance
-          .collection('users')
-          .document(_user.uid)
-          .updateData({'imageUrl': url, 'bio': _controllerBio.text});
+      Firestore.instance.collection('users').document(_user.uid).updateData({
+        'imageUrl': url,
+        'bio': _controllerBio.text,
+        'latitude': _latLng.latitude.toString(),
+        'longitude': _latLng.longitude.toString(),
+        'km': _kmController.text.toString(),
+        'rpg': _rpg ? '1' : '0',
+        'table': _tableGames ? '1' : '0',
+        'safe': _safeSpace ? '1' : '0',
+      });
       var provider = Provider.of<UsersProvider>(context, listen: false);
       provider.updateActiveUser(bio: _controllerBio.text, imageUrl: url);
     } on Firestore catch (error) {
@@ -170,18 +247,40 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     });
   }
 
-  Future<void> _setLocation() async {
+  _checkErroKm() {
+    if (_kmController.text.isEmpty) {
+      _errorsKm = ErrorsKm.ok;
+    } else if (double.tryParse(_kmController.text) == null) {
+      _errorsKm = ErrorsKm.notNumber;
+    } else if (double.parse(_kmController.text) < 1) {
+      _errorsKm = ErrorsKm.outBounds;
+    } else {
+      _errorsKm = ErrorsKm.ok;
+    }
+  }
 
+  Future<void> _setLocation() async {
     await showDialog<String>(
       context: context,
       barrierDismissible: true,
       builder: (BuildContext context) {
         return MapDialog();
       },
-    ).then((result) {
-      setState(() {
-        _tempUrl = result ?? _tempUrl;
-      });
+    ).then((result) async {
+      if (result != null) {
+        _latLng = LatLng(double.parse(result.split('_')[0]),
+            double.parse(result.split('_')[1]));
+        _searchAddress();
+      }
+    });
+  }
+
+  _searchAddress() async {
+    await Geolocator()
+        .placemarkFromCoordinates(_latLng.latitude, _latLng.longitude)
+        .then((address) {
+      _controllerGeo.text =
+          '${address[0].thoroughfare} ${address[0].name}, ${address[0].locality}';
     });
   }
 
@@ -195,6 +294,10 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     }
     if (_user.bio != null) {
       _controllerBio.text = _user.bio;
+    }
+    if (_user.latitude != null) {
+      _latLng = LatLng(_user.latitude, _user.longitude);
+      _searchAddress();
     }
   }
 }
--- iparty/lib/widgets/map-dialog.dart
+++ iparty/lib/widgets/map-dialog.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:geocoder/geocoder.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -16,39 +15,47 @@ class _MapDialogState extends State<MapDialog> {
   LatLng _center = const LatLng(40.974737, -5.672455);
   GoogleMapController _mapController;
   Map<MarkerId, Marker> _marker = <MarkerId, Marker>{};
+  final _searchAddressController = TextEditingController();
+  final double _zoom = 18.0;
+  final _markerId = MarkerId('headquarter');
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    _searchAddressController.dispose();
+    super.dispose();
+  }
 
   _getCurrentLocation() async {
     if (!searchLoc) {
       await Geolocator()
           .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
           .then((Position position) {
-        setState(() {
-          _center = LatLng(position.latitude, position.longitude);
-          _mapController.animateCamera(
-            CameraUpdate.newCameraPosition(
-              CameraPosition(target: _center, zoom: 20.0),
-            ),
-          );
-          final markerId = MarkerId('headquarter');
-          final Marker marker = Marker(
-            markerId: markerId,
-            position: _center,
-            draggable: true,
-            onDragEnd: ((value) {
-              print(value.latitude);
-              print(value.longitude);
-              _center = LatLng(value.latitude, value.longitude);
-            }),
-          );
-
-          setState(() {
-            _marker[markerId] = marker;
-          });
-        });
+        _center = LatLng(position.latitude, position.longitude);
+        _moveMarker(_center);
       });
 
       searchLoc = true;
     }
+  }
+
+  void _moveMarker(LatLng position) {
+    _mapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(target: position, zoom: _zoom),
+    ));
+    final Marker marker = Marker(
+      markerId: _markerId,
+      position: position,
+      draggable: true,
+      onDragEnd: ((value) {
+        _center = LatLng(value.latitude, value.longitude);
+      }),
+    );
+
+    setState(() {
+      _marker[_markerId] = marker;
+    });
   }
 
   void _onCameraMove(CameraPosition position) {
@@ -60,61 +67,93 @@ class _MapDialogState extends State<MapDialog> {
     _getCurrentLocation();
     return Scaffold(
         appBar: AppBar(
-          title: Text("Google Maps"),
+          title: Text("Base de operaciones"),
+          actions: <Widget>[
+            FlatButton(
+              child: Icon(Icons.done),
+              onPressed: _marker.length == 0
+                  ? null
+                  : () => Navigator.pop(context,
+                      '${_marker[_markerId].position.latitude}_${_marker[_markerId].position.longitude}'),
+            )
+          ],
         ),
         body: Stack(
           children: <Widget>[
-            GoogleMap(
-              onMapCreated: (GoogleMapController controller) {
-                _mapController = controller;
-              },
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 20.0,
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: GoogleMap(
+                onMapCreated: (GoogleMapController controller) {
+                  _mapController = controller;
+                },
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: _zoom,
+                ),
+                markers: Set<Marker>.of(_marker.values),
+                onCameraMove: _onCameraMove,
               ),
-              markers: Set<Marker>.of(_marker.values),
-              onCameraMove: _onCameraMove,
             ),
-            _searchingByAddress
-                ? Positioned(
-                    top: 0,
-                    right: 0,
-                    left: 0,
-                    child: Container(
-                      height: 50,
-                      padding: EdgeInsets.all(8),
-                      child: TextField(
+            Positioned(
+              top: 0,
+              right: _searchingByAddress ? 0 : (double.infinity),
+              left: 0,
+              child: Container(
+                height: 60,
+                padding: EdgeInsets.all(8),
+                child: TextField(
+                  controller: _searchAddressController,
+                  onSubmitted: (_) => _searchByAddress(),
 //                    onChanged: onTextChange,
-                        decoration: InputDecoration(
-                            fillColor: Colors.black.withOpacity(0.1),
-                            filled: true,
-                            prefixIcon: Icon(Icons.search),
-                            suffixIcon: IconButton(
+                  decoration: InputDecoration(
+                      hintText: 'Calle, número, código postal...',
+                      hintStyle: TextStyle(color: Colors.black),
+                      fillColor: Colors.black.withOpacity(0.1),
+                      filled: true,
+                      suffixIcon: _searchingByAddress
+                          ? IconButton(
                               icon: Icon(Icons.cancel),
                               onPressed: () {
                                 setState(() => _searchingByAddress = false);
+                                _searchAddressController.text = '';
                               },
-                            ),
-//                      hintText: 'Search something ...',
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none),
-                            contentPadding: EdgeInsets.zero),
-                      ),
-                    ),
-                  )
-                : FloatingActionButton(
-                    child: Icon(Icons.search),
-                    onPressed: () {
-                      setState(() => _searchingByAddress = true);
-                    },
-                  ),
+                            )
+                          : null,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none),
+                      contentPadding: EdgeInsets.only(left: 35)),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              child: Icon(Icons.search),
+              onPressed: () {
+                if (!_searchingByAddress) {
+                  setState(() => _searchingByAddress = true);
+                } else {
+                  _searchByAddress();
+                }
+              },
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              focusElevation: 0,
+              hoverElevation: 0,
+              highlightElevation: 0,
+              disabledElevation: 0,
+            ),
           ],
         ));
   }
-}
 
-//    print('entra');
-//    var addresses = await Geocoder.local.findAddressesFromQuery(_searchAddress);
-//    var first = addresses.first;
-//    print('Localización ${first.featureName} : ${first.coordinates}');
+  void _searchByAddress() async {
+    String address = _searchAddressController.text;
+    await Geolocator()
+        .placemarkFromAddress(address)
+        .then((List<Placemark> placemarks) {
+      _moveMarker(LatLng(
+          placemarks[0].position.latitude, placemarks[0].position.longitude));
+    });
+  }
+}
